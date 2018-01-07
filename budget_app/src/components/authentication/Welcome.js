4ff5bb439451e3afd23bf8082ae6003b26cfc6ba
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2018-01-07 17:33:53 -0500

    Update app title

--- budget_app/src/components/authentication/Welcome.js
+++ budget_app/src/components/authentication/Welcome.js
@@ -6,7 +6,7 @@ const Welcome = (props) => {
 
   return (
     <div>
-      <h1 style={{color: "#FFF"}} > Welcome to Personal Budget </h1>
+      <h1 style={{color: "#FFF"}} > Welcome to Budget </h1>
       <RaisedButton label="Login" primary fullWidth  onClick={()=>props.toggleComponent('login')} />
       <br /><br />
       <RaisedButton label="Register" primary fullWidth  onClick={()=>props.toggleComponent('register')} />
